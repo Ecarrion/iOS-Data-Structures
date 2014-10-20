502003869922259c06dbe419bc36dcb4f942e4ce
Ernesto Carrion <carrion.silver@gmail.com>
2014-10-19 20:50:23 -0500

    Empry Project

---
+++ Data Structures/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Data Structures
+//
+//  Created by Ernesto Carrión on 10/19/14.
+//  Copyright (c) 2014 Salarion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
